44e131bdf52dd170f3489554d7bdbae7b60ce0dd
Baltazar <bgdevsoft@gmail.com>
2022-05-10 12:46:11 -0500

    Respaldo de proyecto

---
+++ public/JS/conexion.js
@@ -0,0 +1,26 @@
+  // Initialize Cloud Firestore through Firebase
+  firebase.initializeApp({
+    apiKey: "AIzaSyD-ZD1xIl7vFo6-vCWh1RW2Gbrl9kigFYw",
+    authDomain: "dbventashinn.firebaseapp.com",
+    projectId: "dbventashinn"
+  });
+  
+  var db = firebase.firestore();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
